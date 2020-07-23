9640079442e5f72c82bbaf1c62cdee34521b87ee
Reiden <48872777+webr7818@users.noreply.github.com>
2020-07-22 22:32:24 -0700

    Update AotVP Complete.py
    Finished Chapter 1

--- Attack of the Vampire Pizzas Complete/AotVP Complete.py
+++ Attack of the Vampire Pizzas Complete/AotVP Complete.py
@@ -1,32 +1,50 @@
-#Import Libraries
-import pygame
-from pygame import*
+#Set up game
 
-#Initialize pygame
+#Import libraries
+import pygame
+from pygame import *
+
+#Initialize Pygame
 pygame.init()
 
+#---------------------------------------------------
 #Define constant variables
+
+#Define the parameters of the game window
 WINDOW_WIDTH = 900
 WINDOW_HEIGHT = 400
-WINDOW_RES = ((WINDOW_WIDTH, WINDOW_HEIGHT))
+WINDOW_RES = (WINDOW_WIDTH, WINDOW_HEIGHT)
+
+#---------------------------------------------------
+#Load assests
 
 #Create window
 GAME_WINDOW = display.set_mode(WINDOW_RES)
 display.set_caption('Attack of the Vampire Pizzas!')
 
 #---------------------------------------------------
-#Start Main Game Loop
+#Start main game loop
+
+#Game loop
 game_running = True
-#Game Loop
 while game_running:
+
+    #-----------------------------------------------
     #Check for events
+
+    #Checking for and handling events
     for event in pygame.event.get():
+
         #Exit loop on quit
         if event.type == QUIT:
             game_running = False
+
+    #--------------------------------------------
+    #Update display.
     display.update()
 
-#End of main game loop
+#Close main game loop
 #----------------------------------------------------
+
 #Clean up game
 pygame.quit()
